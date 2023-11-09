454ffe4fc28d6b940c6cf008e61ed21aeb4a77e4
Fredrik Nilsson <himske@gmail.com>
2023-11-09 08:47:53 +0100

    Add first template skeleton

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_page(request: HttpRequest) -> HttpResponse:
+    return render(request, "blog/index.html")
+
+
+def posts(request: HttpRequest) -> HttpResponse:
+    pass
+
+
+def post_detail(request: HttpRequest) -> HttpResponse:
+    pass
