50feec5ce78c35a338ee9234701395c7ee95103c
Fredrik Nilsson <himske@gmail.com>
2023-11-10 11:11:51 +0100

    Add dynamic data and 404 page

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -72,16 +72,24 @@ posts = [
     },
 ]
 
+
+def get_date(post: dict) -> date:
+    return post["date"]
+
+
 # Create your views here.
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {"posts": latest_posts})
 
 
 def posts(request: HttpRequest) -> HttpResponse:
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
 def post_detail(request: HttpRequest, slug: str) -> HttpResponse:
-    return render(request, "blog/post-detail.html")
+    post = next(post for post in all_posts if post["slug"] == slug)
+    return render(request, "blog/post-detail.html", {"post": post})
