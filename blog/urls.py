4f5f34c292abc3ed137677d42db6b6cc8c1d6deb
Fredrik Nilsson <himske@gmail.com>
2023-12-06 11:38:40 +0100

    Add read later functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.HomePageView.as_view(), name="home_page"),
     path("posts", views.AllPostsView.as_view(), name="posts_page"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post_detail_page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read_later"),
 ]
